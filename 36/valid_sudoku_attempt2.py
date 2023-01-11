699142654c5beb74b025e2a5456fa22a60a9030b
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-01-11 01:27:41 -0500

    updated optimized 2nd attempt

--- 36/valid_sudoku_attempt2.py
+++ 36/valid_sudoku_attempt2.py
@@ -1,48 +1,6 @@
 from typing import List
 
-def transform_array_to_str(arr: List[str]):
-	return "".join(x for x in arr if x !=".")
-
-def remove_value(value: str, possible_values: List[str]):
-	try:
-		possible_values.remove(value)
-		return True
-	except ValueError:
-		return False
-
-
 def isValidSudoku(board: List[List[str]]) -> bool:
-
-	for row in board:
-		possible_values = ["1","2","3","4","5","6","7","8","9"]
-		for val in row:
-			if val.isnumeric():
-				result = remove_value(val, possible_values)
-				if not result:
-					return False
-
-	for cols in zip(*board):
-		str_repr = transform_array_to_str(cols)
-		set_count = len(set(str_repr))
-		str_count = len(str_repr)
-		if set_count != str_count:
-			return False
-
-	for i in range(3):
-		for j in range(3):
-			row1 = board[3*i][3*j:3*(j+1)]
-			row2 = board[3*i+1][3*j:3*(j+1)]
-			row3 = board[3*i+2][3*j:3*(j+1)]
-			sub_board = row1+row2+row3
-			result =transform_array_to_str(sub_board)
-			hasNoDuplicates = len(result) == len(set(result))
-			if not hasNoDuplicates:
-				return False
-
-	return True
-
-
-def isValidSudoku2(board: List[List[str]]) -> bool:
 	col1, col2, col3, col4, col5, col6, col7, col8, col9 = "", "", "", "", "", "", "", "", ""
 	for i in range(3):
 		row1="".join(board[3*i])
@@ -121,18 +79,9 @@ if __name__ == "__main__":
 		[".","9",".",".",".",".",".",".","7"]
 	]
 
-	result = main(board)
-	print(result)
-
-	result = main(board2)
-	print(result)
-
-	result = main(false_board)
-	print(result)
-
-	print(isValidSudoku2(board))
-	print(isValidSudoku2(board2))
-	print(isValidSudoku2(false_board))
+	print(isValidSudoku(board))
+	print(isValidSudoku(board2))
+	print(isValidSudoku(false_board))
 
 '''
 Time Complexity: O(n^2)
