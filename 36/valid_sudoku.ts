06cbf4ece0a17acdd40f7674919aa2257d33252f
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-01-12 01:11:14 -0500

    Fixed tsc compiler issues

--- 36/valid_sudoku.ts
+++ 36/valid_sudoku.ts
@@ -1,15 +1,50 @@
 export {}
 
-function isValidSudoku(board: Array<string[]>): Boolean {
+const createSetArray = (length: number) => Array(length).fill(0).map(_ => new Set()) as Array<Set<string>>;
+
+function isValidSudoku(board: string[][]): Boolean {
 	let rows: string[] = [];
 	let cols: string[] = [];
 	let sub_board: string[] = [];
 
+	console.log(createSetArray(board.length));
 
 	return true
 }
 
+
 function main() {
+
+	const board: Array<string[]> = [
+		["5","3",".",".","7",".",".",".","."],
+		["6",".",".","1","9","5",".",".","."],
+		[".","9","8",".",".",".",".","6","."],
+		["8",".",".",".","6",".",".",".","3"],
+		["4",".",".","8",".","3",".",".","1"],
+		["7",".",".",".","2",".",".",".","6"],
+		[".","6",".",".",".",".","2","8","."],
+		[".",".",".","4","1","9",".",".","5"],
+		[".",".",".",".","8",".",".","7","9"]
+	]
+
+	const board2: Array<string[]> =	[
+		["8","3",".",".","7",".",".",".","."],
+		["6",".",".","1","9","5",".",".","."],
+		[".","9","8",".",".",".",".","6","."],
+		["8",".",".",".","6",".",".",".","3"],
+		["4",".",".","8",".","3",".",".","1"],
+		["7",".",".",".","2",".",".",".","6"],
+		[".","6",".",".",".",".","2","8","."],
+		[".",".",".","4","1","9",".",".","5"],
+		[".",".",".",".","8",".",".","7","9"]
+	]
+
+	const result = isValidSudoku(board);
+	const result2 = isValidSudoku(board2);
+
+	console.log(result);
+	console.log(result2);
+
 
 }
 
